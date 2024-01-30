a218d1a2e1c2052daf89e2414b22b234ad4fd63c
razieh7 <rmrady758@gmail.com>
2024-01-30 19:27:18 +0330

    started project alibaba

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './component/header'
+import Section from './component/section'
+import Footer from './component/footer'
+
+function App() {
+  return (
+    <main>
+      <Header/>
+      <Section/>
+      <Footer/>
+    </main>
+  )
+}
+
+export default App
